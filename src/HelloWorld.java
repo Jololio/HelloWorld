d842c3562a43450c5f3dee5e2e308d393e18334e
Jololio <josephalabudi@gmail.com>
2016-08-26 14:52:20 -0600

    Changed Stuff
    I changed the quotes, and code cleanup.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,10 +1,8 @@
-
 public class HelloWorld
 	{
-
 		public static void main(String[] args)
 			{
-				System.out.println("I want to kill myself, World.");
+				System.out.println("Kill me please I can't code for shit.");
 			}
 
 	}
