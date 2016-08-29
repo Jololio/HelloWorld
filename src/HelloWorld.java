2b1ef2cc24e8528b0f921d8e53e2e8f7ea131f1c
Jololio <josephalabudi@gmail.com>
2016-08-29 17:06:08 -0600

    Text Change

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,7 @@ public class HelloWorld
 	{
 		public static void main(String[] args)
 			{
-				System.out.println("Kill me please I can't code for shit.");
+				System.out.println("LOL How do I code????");
 			}
 
 	}
