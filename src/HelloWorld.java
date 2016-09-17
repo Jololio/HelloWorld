2f0a264866d2ea6cec3061371ff9d9cb95f0080f
Jololio <josephalabudi@gmail.com>
2016-09-16 20:20:34 -0600

    GG

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,7 +2,8 @@ public class HelloWorld
 	{
 		public static void main(String[] args)
 			{
-				System.out.println("LOL How do I code????");
+				System.out.print("LOL How do I code????");
+				System.out.println(" 2 lines of text?");
 			}
-
+		
 	}
